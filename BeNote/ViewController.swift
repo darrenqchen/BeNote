d2010d567ce2a252e5fd84bdc43f1dc6f12126e7
AnanyaT129 <AnaTadig@gmail.com>
2024-10-13 20:13:59 -0400

    Initial Commit

---
+++ BeNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeNote
+//
+//  Created by MAD on 10/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
